84b2ea669c01b43881c9d84ebe52fe21849d7a93
rahulpenchala97 <rpvirtuoso@gmail.com>
2025-07-09 18:33:20 +0530

    fix: update CORS allowed origins to remove trailing slash

--- backend/app_review_project/settings.py
+++ backend/app_review_project/settings.py
@@ -206,7 +206,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://app-review-ui.onrender.com/",
+    "https://app-review-ui.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
